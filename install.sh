418b79b75feadc0f3bab01c1bfb63c86df0c12dd
Jonas Granquist <jonas.granqvist@gmail.com>
2012-08-30 09:43:12 +0200

    Moved ack and pipe.vim to setup.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,1 @@
 pip install flake8
-
-curl http://betterthangrep.com/ack-standalone > $HOME/bin/ack && chmod 0755 $HOME/bin/ack
-
-curl https://raw.github.com/jakobi/script-archive/master/vim/pipe.vim > $HOME/bin/pipe.vim && chmod 0755 $HOME/bin/pipe.vim
--- setup.sh
+++ setup.sh
@@ -9,3 +9,7 @@ unzip SysinternalsSuite.zip
 rm SysinternalsSuite.zip
 
 git clone https://github.com/rupa/z.git
+
+curl http://betterthangrep.com/ack-standalone > ack && chmod 0755 ack
+
+curl https://raw.github.com/jakobi/script-archive/master/vim/pipe.vim > pipe.vim && chmod 0755 pipe.vim
