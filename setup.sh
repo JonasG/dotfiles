bbc8b81073ed9f6e551bfe795344ba843152c982
Jonas Granquist <jonas.granqvist@gmail.com>
2012-10-04 20:47:57 +0200

    Reordered project so everything ends up in a directory. setup.sh now creates a tgz

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+pushd dotfiles
 rm -rf bin
 mkdir bin
 wget http://download.sysinternals.com/files/SysinternalsSuite.zip
 mv SysinternalsSuite.zip bin
-cd bin
+pushd bin
 unzip SysinternalsSuite.zip
 rm SysinternalsSuite.zip
 
@@ -13,3 +14,9 @@ git clone https://github.com/rupa/z.git
 curl http://betterthangrep.com/ack-standalone > ack && chmod 0755 ack
 
 curl https://raw.github.com/jakobi/script-archive/master/vim/pipe.vim > pipe.vim && chmod 0755 pipe.vim
+
+popd
+popd
+
+tar -czf unixconfig.tgz dotfiles
+cp unixconfig.tgz /home/jonas/configs
